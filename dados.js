e5ef82b6ea27de7e34c9abb5a6ca5473873b56b6
Matheus Cardoso <omatheusncardoso@gmail.com>
2024-09-26 01:23:16 -0400

    Repositorio Web
    Uma aplicação em desenvolvimento para guardar meus códigos!

---
+++ dados.js
@@ -0,0 +1,26 @@
+let dados = [
+    {
+        titulo: "Algoritmo de Otimização",
+        descricao: "O código implementa um problema de otimização para selecionar produtos com base em espaço e custo, utilizando os algoritmos de Hill Climbing, Simulated Annealing e Genetic Algorithm da biblioteca mlrose-hiive. Os produtos são definidos com seus respectivos nomes, espaço ocupado e custo. A função de fitness avalia soluções, penalizando aquelas que excedem o espaço disponível. O código busca encontrar a combinação de produtos que maximize o custo total sem ultrapassar o espaço disponível e imprime as melhores soluções encontradas por cada algoritmo.",
+        link: "https://github.com/MatheusC-py/Scripts_IA/blob/main/Algoritmo_Otimizacao.py",
+        tags: "otimização algoritmo custo produtos"
+    },
+    {
+        titulo: "Algoritmo A*",
+        descricao: "O código implementa o algoritmo A* para encontrar o caminho mais curto em um grafo representando cidades e estradas entre elas. Ele define classes para representar vértices (cidades), adjacentes (estradas) e o grafo em si, onde cada cidade tem um nome, uma distância até o objetivo e uma lista de adjacências. A classe AEstrela é responsável por buscar o caminho mais curto a partir de um vértice atual até um vértice objetivo, visitando cidades adjacentes e mantendo uma lista ordenada dessas adjacências com base na distância estimada. O algoritmo imprime as cidades visitadas e os custos das estradas percorridas até encontrar o destino desejado.",
+        link: "https://github.com/MatheusC-py/Scripts_IA/blob/main/Algoritmo_Aestrela.py",
+        tags: "algoritmo A* busca caminho grafo"
+    },
+    {
+        titulo: "App de Metas",
+        descricao: "Este código implementa um aplicativo de gerenciamento de metas, permitindo ao usuário cadastrar, listar, realizar, visualizar e deletar metas. As metas são armazenadas em um arquivo JSON, e o aplicativo utiliza a biblioteca Inquirer para interações no terminal. O usuário pode marcar metas como concluídas, visualizar metas realizadas e abertas, e deletar metas conforme necessário. As alterações nas metas são salvas automaticamente após cada operação e ao sair do aplicativo.",
+        link: "https://github.com/MatheusC-py/Aplicativo-de-lembretes",
+        tags: "gerenciamento metas aplicativo"
+    },
+    {
+        titulo: "FAMETRO PROJETO - Jogos e Controle de Volume",
+        descricao: "Este código implementa um aplicativo interativo que permite ao usuário escolher entre três jogos: Jogo da Velha, Jogo da Cobrinha e um controle de volume utilizando gestos das mãos. O Jogo da Velha é jogado no terminal, onde os jogadores escolhem suas posições no tabuleiro. O Jogo da Cobrinha é uma aplicação gráfica desenvolvida com Pygame, onde o jogador controla uma cobra que deve comer comida e evitar colisões. O controle de volume utiliza a biblioteca OpenCV e Mediapipe para detectar a posição dos dedos da mão, ajustando o volume do sistema de áudio em tempo real. O usuário pode sair do programa a qualquer momento.",
+        link: "https://github.com/MatheusC-py/FamTech", 
+        tags: "jogos pygame controle de volume mediapipe"
+    }
+];
